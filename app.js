86411dc45f6390ea09c8a06b452428fef7d0a28e
Bettina98 <beona67@gmail.com>
2023-11-14 00:36:16 -0300

    corrección orientación navbar

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 
-app.listen(PORT, console.log(`Server running in http://localhost${PORT}`));
+app.listen(PORT, console.log(`Server running in http://localhost:${PORT}`));
